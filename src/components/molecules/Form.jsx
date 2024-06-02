dec1b3a15cd0c37d9f7454c2549279f57bb4d853
Kevin Carter <console.logkevin@gmail.com>
2024-06-01 23:24:22 -0400

    add new dom structure for tablet view

--- src/components/molecules/Form.jsx
+++ src/components/molecules/Form.jsx
@@ -31,39 +31,41 @@ const Form = ({ onRequest }) => {
    }, [formData, isSubmitting]);
    
    return (
-       <form className='form--container' onSubmit={handleSubmit}> 
+      <form className='form--container' onSubmit={handleSubmit}> 
          <img className="form--logo" src={Logo} />
+         
+         <div className="form--input-case">
+            <h1 className="form--heading">Welcome!</h1>
 
-         <h1 className="form--heading">Welcome!</h1>
+            <Input 
+               className={"input--employee-id"}
+               name={"employeeId"} 
+               placeholder={"Employee Id"}
+               labelClass={"input--label-employee-id"}
+               title={"The numeric value that was assigned for identification"}
+               onChange={handleOnChange}
+               />
+            <Input 
+               className={"input--password"}
+               type={"password"}
+               name={"password"} 
+               placeholder={"Password"}
+               labelClass={"input--label-password"}
+               title={"A secure set of characters"}
+               onChange={handleOnChange}
+               /> 
 
-         <Input 
-            className={"input--employee-id"}
-            name={"employeeId"} 
-            placeholder={"Employee Id"}
-            labelClass={"input--label-employee-id"}
-            title={"The numeric value that was assigned for identification"}
-            onChange={handleOnChange}
-            />
-         <Input 
-            className={"input--password"}
-            type={"password"}
-            name={"password"} 
-            placeholder={"Password"}
-            labelClass={"input--label-password"}
-            title={"A secure set of characters"}
-            onChange={handleOnChange}
-         /> 
+            <Button 
+               className={"button--login"}
+               type={"submit"}
+               title={"log into vendex"}
+               text={isSubmitting ? "Loading..." : "Login"}
+               disabled={isSubmitting}
+               />
 
-         <Button 
-            className={"button--login"}
-            type={"submit"}
-            title={"log into vendex"}
-            text={isSubmitting ? "Loading..." : "Login"}
-            disabled={isSubmitting}
-         />
-
-         <Link className="form--trouble">having trouble?</Link>
-       </form>
+            <Link className="form--trouble">having trouble?</Link>
+         </div>
+      </form>
    );
 };
 
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import Form from "../molecules/Form";
 
 const Login = () => {
-   const [formStatus, setFormStatus] = useState(false)
+   const [formStatus, setFormStatus] = useState(false);
+   
    const handleRequest = (formData, isSubmitting) => {
       setFormStatus(isSubmitting); 
       console.log(formData, isSubmitting)
