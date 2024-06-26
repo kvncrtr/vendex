4b98d3924c1d4e79348ad9d1f7778d674ddd05c7
Kevin Carter <console.logkevin@gmail.com>
2024-06-26 17:37:49 -0400

    change endpoint to login

--- src/components/molecules/Form.jsx
+++ src/components/molecules/Form.jsx
@@ -31,7 +31,7 @@ const Form = () => {
       
       // fetch all employee information 
       try {
-         const response = await fetch("http://localhost:8080/employee", {
+         const response = await fetch("http://localhost:8080/login", {
             method: "POST",
             headers: {
                'Content-Type': 'application/json'
