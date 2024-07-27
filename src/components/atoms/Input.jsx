f6c928c649a96759983d128fd52666d13afe7af5
Kevin Carter <console.logkevin@gmail.com>
2024-07-27 07:09:56 -0400

    remove spin button from input fields with type number

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -1,17 +1,17 @@
 import React, { forwardRef } from "react";
 
-const Input = forwardRef((props, ref) => {
+const Input = forwardRef(({type, className, name, title, placeholder, onChange, value}, ref) => {
    return (
       <div className="input--container">
          <input 
-            type={props.type}
-            className={props.className}
-            name={props.name}
-            title={props.title}
-            placeholder={props.placeholder}
+            type={type}
+            className={className}
+            name={name}
+            title={title}
+            placeholder={placeholder}
             ref={ref}
-            onChange={props.onChange}
-            value={props.value}
+            onChange={onChange}
+            value={value}
          />
       </div>
    );
