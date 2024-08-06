848544d007990985b2242bb92a476598ef6548e7
Kevin Carter <console.logkevin@gmail.com>
2024-08-05 21:14:34 -0400

    catch tablet syles

--- src/App.js
+++ src/App.js
@@ -12,11 +12,13 @@ import Transfers from "./components/pages/Transfers";
 import Reports from "./components/pages/Reports";
 import Help from "./components/pages/Help";
 import Settings from "./components/pages/Settings";
+import Sidebar from './components/organisims/Sidebar';
 
 function App() {
   return (
     <>
-      <Routes>
+      <Sidebar />
+      {/* <Routes>
         <Route path="/login" element={<Login />} replace />
         <Route path="*" element={<Login />} replace />
 
@@ -31,96 +33,9 @@ function App() {
             <Route path="settings" element={<Settings />} />
           </Route>
         </Route> 
-      </Routes>
+      </Routes>*/}
     </>
   );
 }
 
 export default App;
-
-/*
-
-once logged in I want to show the sidebar component with the home page 
-how can I do that?
-
-import './style.css'
-import { Route, Routes } from "react-router-dom";
-
-import ProtectedRoutes from './components/organisims/ProtectedRoute';
-import Login from './components/pages/Login';
-import Home from './components/pages/Home';
-
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route element={<ProtectedRoutes />}>
-          <Route path="/" element={<Home />} />
-        </Route> 
-
-        <Route path="/login" element={<Login />} replace />
-        <Route path="*" element={<Login />} replace />
-      </Routes>
-    </>
-  );
-}
-
-export default App;
-
-here is the sidebar
-
-import { useState } from "react";
-
-import { List } from "@phosphor-icons/react";
-import logo from "../../assets/mim-logo.png";
-import Menu from "../molecules/Menu";
-
-const Sidebar = () => {
-   const [showMenu, setShowMenu] = useState(false);
-
-   const handleClick = () => {
-      setShowMenu(!showMenu);
-   }
-
-   return (
-      <div className="sidebar--container">
-         <div className="sidebar--case">
-            <div className="sidebar--logo-case">
-               <img className="sidebar--logo" src={logo}></img>
-            </div>
-
-            <div  onClick={handleClick} className="sidebar--burger-icon">
-               <List weight="thin" className="sidebar--list-icon" size={24} />
-            </div>
-         </div>
-
-         {showMenu && <Menu changeState={handleClick} />}
-      </div>
-   );
-};
-
-export default Sidebar;
-
-here is home
-
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchEmployeeById } from "../../store/employee";
-
-const Home = () => {
-   const dispatch = useDispatch();
-   const employeeId = useSelector(state => state.auth.current_employee.employee_id);
-   const current_employee = useSelector(state => state.auth.current_employee);
-
-   useEffect(() => { if (employeeId) { dispatch(fetchEmployeeById(employeeId)) }}, []);
-
-   return (
-      <div>
-            <h1>HOME</h1>
-      </div>
-   );
-};
-
-export default Home;
-
-*/ 
