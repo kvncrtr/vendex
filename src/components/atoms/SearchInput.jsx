93907ad07547d1fe619dd0b9a7130dd1b44bb641
Kevin Carter <console.logkevin@gmail.com>
2024-08-09 07:15:44 -0400

    add search bar styles

---
+++ src/components/atoms/SearchInput.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SearchInput = () => {
+   return (
+      <input 
+         className="searchinput--field"
+         placeholder="Search"
+      >
+      
+      </input>
+   );
+};
+
+export default SearchInput;
--- src/components/molecules/Searchbar.jsx
+++ src/components/molecules/Searchbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/mim-logo.png";
+import SearchInput from "../atoms/SearchInput";
+import { MagnifyingGlass } from "@phosphor-icons/react";
 
 const Searchbar = () => {
   return (
     <div className="searchbar--container">
-      <p>Searchbar</p>
+      <img className="searchbar--logo" src={logo}></img>
+
+      <div className="searchbar--input-case">
+        <MagnifyingGlass className={"searchbar--search-button"} weight="thin" size={24} />
+        <SearchInput />
+      </div>
     </div>
   )
 }
