89a26e603b1fdbc1fc43ff3e6eefc1b26fd82cd1
Kevin Carter <console.logkevin@gmail.com>
2025-05-25 12:27:21 -0400

    change url for login and employees

--- src/services/employee-api.js
+++ src/services/employee-api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const  GetEmployees = async () => {
-   const response = await axios.get("http://localhost:8080/employee")
+   const response = await axios.get("https://vendex-backend.onrender.com/employees")
    return response
 };
 
 const ValidateEmployee = async (body) => {
    try {
-      const response = await axios.post("http://localhost:8080/login", body);
+      const response = await axios.post("https://vendex-backend.onrender.com/login", body);
       return response.data;
    } catch (error) {
       const errorResponse = error.response.data.message;
