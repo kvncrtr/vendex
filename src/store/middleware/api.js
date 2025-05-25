4859aa8870a6a90fe94fa62acdf62dc2767e1e93
Kevin Carter <console.logkevin@gmail.com>
2025-05-25 12:24:56 -0400

    redeploy new instance of backend

--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -5,40 +5,40 @@ const api = ({ dispatch }) => next => async action => {
    if (action.type !== apiCallBegan.type) return next(action);
 
    const {
-      url, 
-      method, 
-      data, 
+      url,
+      method,
+      data,
       headers,
-      onInit, 
-      onFulfilled, 
+      onInit,
+      onFulfilled,
       onRejected,
       onShowError } = action.payload;
-      
-      if (onInit) dispatch({type: onInit, payload: {isLoading: true}});
-   
+
+   if (onInit) dispatch({ type: onInit, payload: { isLoading: true } });
+
    try {
       if (headers) {
          const response = await axios.request({
-            baseURL: "https://vendex-backend-production.up.railway.app/",
+            baseURL: "https://vendex-backend.onrender.com",
             url,
             method,
-            data, 
+            data,
             headers
          });
-         dispatch({ type: onFulfilled, payload: response.data});      
-      } else  {
+         dispatch({ type: onFulfilled, payload: response.data });
+      } else {
          const response = await axios.request({
-            baseURL: "https://vendex-backend-production.up.railway.app/",
+            baseURL: "https://vendex-backend.onrender.com",
             url,
             method,
             data
          });
-         dispatch({ type: onFulfilled, payload: response.data});      
+         dispatch({ type: onFulfilled, payload: response.data });
       }
-      
+
    } catch (error) {
-      dispatch({type: onRejected, payload: {errorMessage: `${error.response.data.message}`}})
-      dispatch({type: onShowError, payload: {errorMessage: `${error.response.data.message}`}})
+      dispatch({ type: onRejected, payload: { errorMessage: `${error.response.data.message}` } })
+      dispatch({ type: onShowError, payload: { errorMessage: `${error.response.data.message}` } })
    }
 }
 
